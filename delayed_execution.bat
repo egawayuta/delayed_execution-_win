b25fd1387e2ac079a0eb2d01e02ba3ee48044c11
egawa <egawayuta@egawayuuta-no-MacBook-Pro.local>
2017-01-30 17:26:50 +0900

    firstcommit

---
+++ delayed_execution.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem
+rem OSが上がってから指定秒後に指定のアプリケーションを叩きます
+rem スタートアップに入れて使用してください
+rem
+
+rem 起動までの時間（秒数）を指定
+set /A delay=10
+
+rem 起動したいアプリケーションの（フル）パスを指定
+set appspath=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+
+rem ------------------------------------------------------
+if exist "%appspath%" (
+  call timeout %delay%
+  call start "" "%appspath%"
+) else (
+  call echo;
+  call echo ---- 指定のファイルが存在しません ----
+  call echo;
+  pause
+)
+exit
