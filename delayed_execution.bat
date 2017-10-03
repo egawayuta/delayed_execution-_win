fcd039e3f6710650c97fe6d4dbfa20622449dbe3
egawayuta <yuta.egawa@gmail.com>
2017-10-03 09:00:30 +0900

    行間を見やすく変更。

--- delayed_execution.bat
+++ delayed_execution.bat
@@ -1,17 +1,18 @@
 @echo off
 
-rem
+rem ------------------------------------------------------
+
 rem OSが上がってから指定秒後に指定のアプリケーションを叩きます
 rem スタートアップに入れて使用してください
-rem
 
 rem 起動までの時間（秒数）を指定
 set /A delay=10
 
 rem 起動したいアプリケーションの（フル）パスを指定
-set appspath=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+set appspath=%windir%\system32\notepad.exe
 
 rem ------------------------------------------------------
+
 if exist "%appspath%" (
   call timeout %delay%
   call start "" "%appspath%"
